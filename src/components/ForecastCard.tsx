b17913f8cb7831a4a532a247da8c73e55732fc62
santiagomac <smazuerac@unac.edu.co>
2024-01-26 17:58:29 -0500

    update the week card component

--- src/components/ForecastCard.tsx
+++ src/components/ForecastCard.tsx
@@ -1,4 +1,4 @@
-import { Forecast } from '../interface';
+import { Forecast } from '../interfaces/interface';
 import { formatDate } from '../utils/date';
 
 const ForecastCard = ({
@@ -16,11 +16,11 @@ const ForecastCard = ({
       </div>
       <div className="grid grid-cols-2 gap-3 mb-4">
         <div className="border-t-4 border-red-400 rounded-lg flex flex-col justify-center items-center shadow-lg p-3">
-          <p className="text-lg font-bold">Min Temp</p>
+          <p className="text-lg font-bold">Max Temp</p>
           <p className="text-md">{forecast?.forecastday[0].day.maxtemp_c}°</p>
         </div>
         <div className="border-t-4 border-red-400 rounded-lg flex flex-col justify-center items-center shadow-lg p-3">
-          <p className="text-lg font-bold">Max Temp</p>
+          <p className="text-lg font-bold">Min Temp</p>
           <p className="text-md">{forecast?.forecastday[0].day.mintemp_c}°</p>
         </div>
       </div>
