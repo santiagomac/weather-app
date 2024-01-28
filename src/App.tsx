dc0fba016161414034be35e8a49918187f1c82bc
santiagomac <smazuerac@unac.edu.co>
2024-01-28 16:04:08 -0500

    update the configuration and clean yarn caché

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import MainCard from './components/MainCard';
 
+import './App.css';
+
 import { useEffect, useState } from 'react';
 import { Weather } from './interfaces/interface';
 import ForecastCard from './components/ForecastCard';
@@ -39,7 +41,7 @@ function App() {
   }, [data]);
 
   return (
-    <div className="mt-10 mx-auto sm:my-auto sm:flex sm:justify-center sm:flex-col sm:items-center sm:mt-14 md:mt-10 lg:mt-0 absolute inset-0 -z-10 h-[2000px] md:h-[1070px] lg:h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#0CD_100%)]">
+    <div className="mt-10 mx-auto sm:my-auto sm:flex sm:justify-center sm:flex-col sm:items-center sm:mt-14 md:mt-10 lg:mt-0 absolute inset-0 -z-10 h-[2000px] md:h-[1070px] lg:h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#0CD_100%)] height-670">
       <div className="max-w-[1200px] p-6">
         <h1 className="text-center text-5xl mt-5 mb-5 font-bold">
           Weather App
